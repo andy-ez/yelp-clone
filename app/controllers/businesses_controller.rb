38edf09d0f6d07d1090334dab360e3859d0bee98
andy-ez <aezeanaka@gmail.com>
2017-02-27 18:19:13 +0000

    show

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -2,4 +2,8 @@ class BusinessesController < ApplicationController
   def index
     @businesses = Business.paginate(page: params[:page], per_page: 10)
   end
+
+  def show
+    @business = Business.find(params[:id])
+  end
 end
