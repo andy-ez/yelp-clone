83665d83edf613556d5b1175ee9ba46be99e0c4a
andy-ez <aezeanaka@gmail.com>
2017-02-28 09:49:47 +0000

    User thumbs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227162523) do
+ActiveRecord::Schema.define(version: 20170228092746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20170227162523) do
     t.string "post_code"
     t.string "password_digest"
     t.string "avatar"
+    t.string "thumb"
   end
 
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,9 +1,12 @@
 Fabricator(:user) do
-  first_name { Faker::Name.first_name }
-  last_name { Faker::Name.last_name }
+  f_name = Faker::Name.first_name
+  l_name = Faker::Name.last_name
+  first_name { f_name }
+  last_name { l_name }
   email { Faker::Internet.email }
   city { Faker::Address.city }
   post_code { Faker::Address.postcode }
   password { "Password" }
-  avatar { Faker::Avatar.image }
+  avatar { Faker::Avatar.image("#{f_name}-#{l_name}", "200x200" ) }
+  thumb { Faker::Avatar.image("#{f_name}-#{l_name}", "55x55" ) }
 end
