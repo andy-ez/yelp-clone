38b28bc39914016bf989fe04bcb551ac0bcfffc9
andy-ez <aezeanaka@gmail.com>
2017-03-01 14:13:27 +0000

    change redirect path for unauthenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def require_user
     unless logged_in?
       flash[:danger] = "You must be logged in for that."
-      redirect_to root_path
+      redirect_to login_path
     end
   end
 
